db0ca679fa48b9249ccb8e5345e1eb3bb7fcff3a
Poltto <martti.kaipiainen@gmail.com>
2024-11-20 16:25:12 +0100

    Fixed authentication redirect issues

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,30 +5,17 @@ import App from './app.tsx';
 import { store } from './stores/store.ts';
 import { Provider } from 'react-redux';
 import { createTheme, MantineProvider } from '@mantine/core';
-import { Auth0Provider } from '@auth0/auth0-react';
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
-const isProduction = import.meta.env.MODE === 'production';
-const clientId = isProduction
-  ? 'vlPaAflsi4YGyYO7ssyEnjrQxvo4GtDs'
-  : 'lqTQ2LfvM5yyicwSy5mCq6nPiVBWSPM7';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <Auth0Provider
-        domain="dev-ev7m3p4bucka4la6.us.auth0.com"
-        clientId={clientId}
-        authorizationParams={{
-          redirect_uri: window.location.origin,
-        }}
-      >
-        <MantineProvider theme={theme}>
-          <App />
-        </MantineProvider>
-      </Auth0Provider>
+      <MantineProvider theme={theme}>
+        <App />
+      </MantineProvider>
     </Provider>
   </StrictMode>,
 );
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,38 +1,87 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { ContentPage } from './pages/content/content.page';
+import { createBrowserRouter, Outlet, useNavigate } from 'react-router-dom';
 import { PlantMinderPage } from './pages/plant-minder/plant-minder.page';
 import type { Router as RemixRouter } from '@remix-run/router/dist/router';
-import { withAuthenticationRequired } from '@auth0/auth0-react';
-import { Box, LoadingOverlay } from '@mantine/core';
+import {
+  Auth0Provider,
+  useAuth0,
+  withAuthenticationRequired,
+} from '@auth0/auth0-react';
 import { ComponentType, FC } from 'react';
+import { LoadingOverlay } from '@mantine/core';
 
 interface AuthenticationGuardProps {
   component: ComponentType; // Ensures `component` is a valid React component
 }
 
-const AuthenticationGuard: FC<AuthenticationGuardProps> = ({ component }) => {
-  const Component = withAuthenticationRequired(component, {
-    onRedirecting: () => (
-      <Box
-        className={
-          'fixed top-0 left-0 w-full h-full flex justify-center items-center'
-        }
-      >
-        <LoadingOverlay
-          zIndex={1000}
-          overlayProps={{ radius: 'sm', blur: 2 }}
-        />
-      </Box>
-    ),
-  });
+const AuthenticationGuard: FC<AuthenticationGuardProps> = ({
+  component,
+  ...props
+}) => {
+  const { isLoading, isAuthenticated } = useAuth0();
+  console.log('HELLO', isLoading, isAuthenticated);
+  if (isLoading) {
+    return (
+      <LoadingOverlay
+        visible={true}
+        zIndex={1000}
+        overlayProps={{ radius: 'lg', blur: 2 }}
+        pos={'absolute'}
+      ></LoadingOverlay>
+    );
+  } else {
+    const Component = withAuthenticationRequired(component);
+    return <Component {...props} />;
+  }
+};
 
-  return <Component />;
+export const Auth0ProviderWithNavigate: FC<{ children: any }> = ({
+  children,
+}) => {
+  const navigate = useNavigate();
+  const isProduction = import.meta.env.MODE === 'production';
+
+  const domain = 'dev-ev7m3p4bucka4la6.us.auth0.com';
+  const clientId = isProduction
+    ? 'vlPaAflsi4YGyYO7ssyEnjrQxvo4GtDs'
+    : 'lqTQ2LfvM5yyicwSy5mCq6nPiVBWSPM7';
+  const redirectUri = 'http://localhost:4200';
+
+  const onRedirectCallback = (appState: any) => {
+    console.log('REDIRECT', appState);
+    navigate(appState?.returnTo || window.location.pathname);
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
 };
 
 export const routes: RemixRouter = createBrowserRouter([
   {
-    path: '/',
-    element: <ContentPage />,
+    path: '',
+    element: (
+      <Auth0ProviderWithNavigate>
+        <Outlet />
+      </Auth0ProviderWithNavigate>
+    ),
+    errorElement: (
+      <h1
+        className={
+          'w-full h-full fixed top-0 left-0 flex justify-center items-center'
+        }
+      >
+        404 - Page not found
+      </h1>
+    ),
     children: [
       {
         path: 'plant-minder',
