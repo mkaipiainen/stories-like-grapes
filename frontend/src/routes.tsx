e8b74d5cf92c2550d9b2a4e6a9c27bbfe69492c1
Poltto <martti.kaipiainen@gmail.com>
2024-11-20 02:11:26 +0100

    Added authentication and protected routes

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,6 +2,32 @@ import { createBrowserRouter } from 'react-router-dom';
 import { ContentPage } from './pages/content/content.page';
 import { PlantMinderPage } from './pages/plant-minder/plant-minder.page';
 import type { Router as RemixRouter } from '@remix-run/router/dist/router';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+import { Box, LoadingOverlay } from '@mantine/core';
+import { ComponentType, FC } from 'react';
+
+interface AuthenticationGuardProps {
+  component: ComponentType; // Ensures `component` is a valid React component
+}
+
+const AuthenticationGuard: FC<AuthenticationGuardProps> = ({ component }) => {
+  const Component = withAuthenticationRequired(component, {
+    onRedirecting: () => (
+      <Box
+        className={
+          'fixed top-0 left-0 w-full h-full flex justify-center items-center'
+        }
+      >
+        <LoadingOverlay
+          zIndex={1000}
+          overlayProps={{ radius: 'sm', blur: 2 }}
+        />
+      </Box>
+    ),
+  });
+
+  return <Component />;
+};
 
 export const routes: RemixRouter = createBrowserRouter([
   {
@@ -10,7 +36,7 @@ export const routes: RemixRouter = createBrowserRouter([
     children: [
       {
         path: 'plant-minder',
-        element: <PlantMinderPage />,
+        element: <AuthenticationGuard component={PlantMinderPage} />,
       },
     ],
   },
