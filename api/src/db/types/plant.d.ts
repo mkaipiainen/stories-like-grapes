7ab74b096170e0d851e44f8559a7cf62203165a0
Poltto <martti.kaipiainen@gmail.com>
2024-12-07 19:34:23 +0100

    Fixed faulty type definitions

--- api/src/db/types/plant.d.ts
+++ api/src/db/types/plant.d.ts
@@ -1,11 +1,11 @@
-import { ColumnType, Generated, Selectable } from 'kysely';
+import { ColumnType, Generated, GeneratedAlways, Selectable } from 'kysely';
 
 export type PlantTable = {
-  id: Generated<number>;
+  id: GeneratedAlways<number>;
   name: string;
   watering_frequency: number | undefined;
-  date_created: Generated<ColumnType<Date, string, never>>;
-  date_updated: Generated<ColumnType<Date, string, never>>;
+  date_created: ColumnType<Date, never, never>;
+  date_updated: ColumnType<Date, never, never>;
   last_watered: ColumnType<Date | undefined, string | undefined, string | undefined>;
   description: string | undefined;
 };
@@ -14,10 +14,12 @@ export type PlantTagTable = {
   id: Generated<number>;
   plant_id: number;
   name: string;
-  date_created: Generated<ColumnType<Date, string, never>>;
+  date_created: ColumnType<Date, never, never>;
 }
 
 export type Plant = Selectable<PlantTable> & {
-  tags: string[];
+  tags: {
+    name: string;
+  }[];
 };
 export type PlantTag = Selectable<PlantTagTable>;
