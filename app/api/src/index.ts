92f8fefb8b08cd3f5aedcea8bc34881102a8c0b9
Poltto <martti.kaipiainen@gmail.com>
2024-12-11 09:32:05 +0100

    Added cors to local deployment

--- app/api/src/index.ts
+++ app/api/src/index.ts
@@ -1,9 +1,12 @@
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { appRouter } from './router';
 import express from 'express';
-
+import cors from 'cors';
 const app = express();
 const isProduction = process.env.ENVIRONMENT === 'production';
+if(!isProduction) {
+    app.use(cors());
+}
 // created for each request
 const createContext = () => ({}) // no context
 app.use(
