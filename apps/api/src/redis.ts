abdeb094d03e3a2ca71eab037da787d43b37c4dd
Poltto <martti.kaipiainen@gmail.com>
2025-01-07 10:43:14 +0100

    Fixed redis connection info

--- apps/api/src/redis.ts
+++ apps/api/src/redis.ts
@@ -2,6 +2,6 @@ import { Redis } from 'ioredis';
 const isProduction = process.env.ENVIRONMENT === 'production';
 
 export const redis = new Redis({
-  host: isProduction ? 'redis' : 'localhost',
+  host: isProduction ? 'slg-redis' : 'localhost',
   password: undefined,
 });
